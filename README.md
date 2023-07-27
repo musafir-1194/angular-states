ef66527bdb4164b3850c0e04c03133599fd3dde7
musafir-1194 <sagarpanwar0123@gmail.com>
2023-07-27 15:49:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-states
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/g5nhxo)
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './counter.reducer';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [BrowserModule, StoreModule.forRoot({ count: counterReducer })],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
+
+/*
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://github.com/ngrx/platform
+*/
